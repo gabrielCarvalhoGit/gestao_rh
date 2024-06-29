feea9ebf497760929a116d967e1c144036fb451b
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2024-06-29 01:16:15 -0300

    Exposing API Django Rest Framework

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from django.contrib.auth.models import Group, User
+from .serializers import UserSerializer, GroupSerializer
 from django.contrib.auth.decorators import login_required
-from apps.funcionarios.models import Funcionario
 
 
 @login_required
@@ -8,3 +10,13 @@ def home(request):
     data = {}
     data['usuario'] = request.user
     return render(request, 'core/index.html', data)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.conf import settings
+from rest_framework import routers
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from apps.core import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +20,7 @@ urlpatterns = [
     path('documentos/', include("apps.documentos.urls")),
     path('horas-extras/', include("apps.registro_horas_extras.urls")),
     path('accounts/', include("django.contrib.auth.urls")),
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
